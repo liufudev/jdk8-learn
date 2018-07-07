18debb12722369b72ce0f12048d20e9c174fc413
liufu <liufu_dev@qq.com>
2018-07-07 10:21:00 +0800

    add

--- src/test/java/OptionalTest.java
+++ src/test/java/OptionalTest.java
@@ -1,12 +1,15 @@
 import com.google.common.collect.Lists;
+import org.junit.Test;
 import org.lf.pojo.Car;
 import org.lf.pojo.Insurance;
 import org.lf.pojo.Person;
 
 import java.util.ArrayList;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.ToIntFunction;
 import java.util.stream.Stream;
 
 public class OptionalTest {
@@ -19,6 +22,7 @@ public class OptionalTest {
                         .map(Insurance::getName);
     }
 
+    @Test
     public void test1() {
         ArrayList<Integer> list1 = Lists.newArrayList();
         list1.add(1);
@@ -32,7 +36,19 @@ public class OptionalTest {
         list1.add(10);
         ArrayList<ArrayList<Integer>> result = Lists.newArrayList();
         //Function<? super T, ? extends Stream<? extends R>> mapper
-        Function<List<Integer>, Stream<Integer>> mapper= a->a.stream();
+        Function<List<Integer>, Stream<Integer>> mapper = a -> a.stream();
         result.stream().flatMap(mapper);
+    }
+
+    @Test
+    public void test2() {
+        ArrayList<String> list1 = Lists.newArrayList();
+        list1.add("1");
+        list1.add("2");
+        list1.add("3");
+        list1.add("4");
+        ToIntFunction<String> mapper = a -> Integer.valueOf(a);
+        list1.stream().mapToInt(mapper);
+        IntSummaryStatistics intSummaryStatistics = list1.stream().mapToInt(Integer::valueOf).summaryStatistics();
     }
 }
